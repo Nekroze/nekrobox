5a7c2daf45e0da8420f4e1a32234d93644591f4d
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-08-22 22:05:16 +1000

    Moved DocParameters to a function based decorator called params

--- nekrobox/docdecs.py
+++ nekrobox/docdecs.py
@@ -6,29 +6,40 @@ import six
 from six.moves import map
 
 
-class DocParameters(object):
+def params(**argtypes):
     """
-    Automatically document parameters for a function or method.
-    """
+    A decorator that automatically adds parameter and return type tags to a
+    function or method docstring.
 
-    def __init__(self, **argtypes):
-        def _paramline(nametypedoc):
+    Input should be specified as the parameter keyword/name and then a tuple of
+    its type and a description string. This will also use the `returns` keyword
+    to identify the returned type and documentation. The following is an
+    example of how to use `docparams` to automatically document arguments::
+
+        @docparams(input=(str, "string to be made lowercase.")
+                   returns=(str, "lowercase string"))
+        def lowercase(input):
+            return input.lower()
+    """
+    def modify(function):
+        def paramline(nametypedoc):
             """Takes (name, (type, doc)) converts to a param docstring line."""
-            name, typedoc = nametypedoc
-            return ":param {1} {0}: {2}".format(name, *typedoc)
-        rtype, rdoc = argtypes.pop('return', (None, None))
-        paramlines = map(_paramline, six.iteritems(argtypes))
+            name, (atype, doc) = nametypedoc
+            return ":param {1} {0}: {2}".format(name, atype.__name__, doc)
+
+        rtype, rdoc = argtypes.pop("returns", (None, None))
+        paramlines = list(map(paramline, six.iteritems(argtypes)))
 
         if rdoc:
             paramlines.append(":return: {0}".format(rdoc))
         if rtype:
             paramlines.append(":rtype: {0}".format(rtype))
 
-        self.doc = '\n'.join(paramlines)
+        doc = '\n'.join(paramlines)
 
-    def __call__(self, function):
         if function.__doc__:
-            function.__doc__ = self.doc + function.__doc__
+            function.__doc__ = doc + function.__doc__
         else:
-            function.__doc__ = self.doc
+            function.__doc__ = doc
         return function
+    return modify
--- test/test_docdecs.py
+++ test/test_docdecs.py
@@ -11,9 +11,14 @@ from nekrobox import docdecs
 
 
 class TestNekrobox(object):
-    def test_DocParameters(self):
-        @docdecs.DocParameters(word=('str', 'echo this word'))
+    def test_params(self):
+        @docdecs.params(word=(str, 'echo this word'))
         def echo(word):
             return word
+
+        @docdecs.params(input=(str, 'input to make lowercase'),
+                        returns=(str, 'lowercase version of input'))
+        def lowercase(text):
+            return text.lower()
 
         assert echo.__doc__ == ":param str word: echo this word"
