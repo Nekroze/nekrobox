54bbf56f91d5eb12c28dbe1738f4112d1e7b904c
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-09-22 21:04:25 +1000

    update to version 0.3.0

--- nekrobox/__init__.py
+++ nekrobox/__init__.py
@@ -3,4 +3,4 @@ A package containing multiple general use utilities for free use.
 """
 __author__ = 'Taylor "Nekroze" Lawson'
 __email__ = 'nekroze@eturnilnetwork.com'
-__version__ = '0.2.4'
+__version__ = '0.3.0'
